c1ff32193e6988caeef72239e2344d42792c2952
tonio155 <tonio155@gmail.com>
2016-08-05 13:53:31 +0100

    v0.1.1 authentication implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'static/homepage'
+  devise_for :users
+  root 'static#homepage'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
